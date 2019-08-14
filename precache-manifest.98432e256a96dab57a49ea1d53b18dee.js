262be0456b3cf1a34d0bc72615a0436fb1333ebe
Hardik Solanki <hardik16solanki@gmail.com>
2019-08-14 11:52:37 +0530

    Updates

---
+++ precache-manifest.98432e256a96dab57a49ea1d53b18dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ef2a6a0733eb9af03e91167e898b00",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "c2f02ab011dd799ba1a6",
+    "url": "/monstor-rolodex/static/css/main.5be97ba2.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monstor-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "c2f02ab011dd799ba1a6",
+    "url": "/monstor-rolodex/static/js/main.c32e3922.chunk.js"
+  },
+  {
+    "revision": "7142431cf660db093442",
+    "url": "/monstor-rolodex/static/js/runtime~main.3f9f3c61.js"
+  }
+]);
